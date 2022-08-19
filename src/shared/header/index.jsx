40c54c91cdd179cdc46f7f669ffa6315d731c0e8
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-08-19 02:08:36 +0200

    haciendo el header

--- src/shared/header/index.jsx
+++ src/shared/header/index.jsx
@@ -4,14 +4,27 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='wrap__header d-flex'>
-<div className="header__options col-4 d-flex justify-content-start align-items-center ps-1">
+    <div className='wrap__header d-flex border'>
+<div className="header__options col-4 d-flex justify-content-start pt-3 ps-2">
     <div className="options d-flex align-items-center">
         <div className="option__theme"></div>
         <div className="option_lenguage d-flex align-items-center">English</div>
     </div>
 </div>
-<div className="header__links col-8 bg-success">Link
+<div className="header__links col-8 mt-4">
+    
+    <div className="link__slice d-flex justify-content-around w-100 align-items-end m-0 pt-4">
+        <div className="link">Acerca de Mi</div>
+        <div className="link">Proyectos</div>
+        <div className="link">Contacto</div>
+    </div>
+    
+    <div className="link__hr my-2">
+ <hr className='hrs my-1 w-100' />
+
+ <hr className='hri my-0 w-75' />
+
+    </div>
 
 </div>
     </div>
