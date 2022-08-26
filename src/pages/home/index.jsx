5fa94790c635b4c30f19bff3f770f2828eeb0f79
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-08-26 21:57:11 +0200

    boton leer mas o menos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,18 +3,21 @@ import React, { useState } from 'react'
 
 const Home = () => {
 
-const [leer, setLeer] = useState('');
+const [read, setRead] = useState('');
+const [more, setMore] = useState('Leer Mas...')
 
 
 
 
 function changeLeer(){
-  if(leer===''){
-    setLeer('none')
- console.log(leer)
+  if(read===''){
+    setRead('none')
+    setMore('Leer Mas...')
+ console.log(read)
 }else{
-  setLeer('')
- console.log(leer)
+  setRead('')
+  setMore('Leer Menos...')
+ console.log(read)
 }
 }
 
@@ -26,12 +29,12 @@ function changeLeer(){
             <div className="extract__about px-4 py-2 border">
             <p>Hola! Mi nombre es Luis soy Ingeniero en Mantenimiento Industrial, soy un apasionado de la tecnología, poseo una mente inquieta y creativa, buscando reinventarme he descubierto mi gusto por la programación y ha despertado mi deseo de aprender y trabajar en este campo tan amplio y poderoso que tendrá un impacto determinante en el desarrollo de nuestra civilización.</p>
             
-            <p style={{display:`${leer}`}}>Durante el último año he venido aprendiendo de manera autodidacta tecnologías y herramientas relacionadas específicamente con el desarrollo web, además recientemente participé en un Bootcamp donde he adquirido un cumulo nuevos conocimientos y habilidades que me han motivado a seguir creciendo en conocimientos y experiencia.</p>
+            <p style={{display:`${read}`}}>Durante el último año he venido aprendiendo de manera autodidacta tecnologías y herramientas relacionadas específicamente con el desarrollo web, además recientemente participé en un Bootcamp donde he adquirido un cumulo nuevos conocimientos y habilidades que me han motivado a seguir creciendo en conocimientos y experiencia.</p>
             
-            <p style={{display:`${leer}`}}>Actualmente tengo conocimientos de: HTML, CSS, JavaScript, REACT JS, BOOTSTRAP, SASS, GIT, GITHUB, SOURCETREE, Metodología BEM, he desarrollado algunos proyectos que presento a continuación.</p>
+            <p style={{display:`${read}`}}>Actualmente tengo conocimientos de: HTML, CSS, JavaScript, REACT JS, BOOTSTRAP, SASS, GIT, GITHUB, SOURCETREE, Metodología BEM, he desarrollado algunos proyectos que presento a continuación.</p>
             
             <div className="wrap__btn__leerMas d-flex justify-content-end">
-            <button className='btn btn__leerMas border' onClick={changeLeer}>Leer mas...</button>
+            <button className='btn btn__leerMas border bg-secondary' onClick={changeLeer}>{more}</button>
             </div>
 
             </div>
