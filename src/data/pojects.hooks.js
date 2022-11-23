07245a9b82b86f58d6bb2e29647c7fccc6751380
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-11-23 18:55:16 +0100

    agregando proyectos CryptoCalculator y ControlGastos

--- src/data/pojects.hooks.js
+++ src/data/pojects.hooks.js
@@ -5,74 +5,58 @@ import barralibre45 from "../image/fotoBarraLibre45.png";
 import zuccherosCakes from "../image/fotoZuccherosCakes.png"
 import fotoTikTacApp from "../image/fotoTikTacApp.png"
 import fotoEuroMecanica from "../image/fotoEuroMecanica.png"
+import fotoControlGastos from "../image/fotoControlGastos.png"
+import fotoCryptoCalculator from "../image/fotoCryptoCalculator.png"
 
 const useProjects = () => {
 
   const miProjects = [
     {
       "id": 1,
-      "title": "EuroMecánica",
-      "description":
-        "EuroMecanica es un sistema de gestión completo para un taller de mecánica automotriz, donde el usuario podrá crear fichas particulares para cada vehículo recibido para su mantenimiento quedando esto alojado temporalmente en el localStorage, en este sistema, el usuario podrá generar tanto presupuestos como facturas.",
-      "tecnologies": "HTML, CSS, JavaScript, REACT y TAILWIND.",
-      "url": "https://talleres-euromecanica.netlify.app/",
-      "github": "https://github.com/Luis1Gonzalez/euroMecanica",
-      "picture": fotoEuroMecanica,
+      "title": "CryptoCalculator",
+      "picture": fotoCryptoCalculator,
     },
 
     {
       "id": 2,
+      "title": "Control Gastos",
+      "picture": fotoControlGastos,
+    },
+
+    {
+      "id": 3,
+      "title": "EuroMecánica",
+      "picture": fotoEuroMecanica,
+    },
+
+    {
+      "id": 4,
       "title": "Weather App",
-      "description":
-        "Es un aplicación que muestra el estado del tiempo en real time, sea en local o de la ciudad ingresada por el usuario, esta app extrae la información climatologica haciendo uso de la API OPENWEATHER.",
-      "tecnologies": "HTML, CSS, JavaScript y REACT.",
-      "url": "https://weather-1-app.netlify.app",
-      "github": "https://github.com/Luis1Gonzalez/weather-app.git",
       "picture": fotoWeatherApp,
     },
 
 
     {
-      "id": 3,
+      "id": 5,
       "title": "TIK-TAC",
-      "description":
-        "Es un aplicación que muestra el estado del tiempo en real time, sea en local o de la ciudad ingresada por el usuario, esta app extrae la información climatologica haciendo uso de la API OPENWEATHER.",
-      "tecnologies": "HTML, CSS, JavaScript y REACT.",
-      "url": "https://tik-tac.netlify.app",
-      "github": "https://github.com/Luis1Gonzalez/tik-tac",
       "picture": fotoTikTacApp,
     },
 
     {
-      "id": 4,
+      "id": 6,
       "title": "LaCartelera.es",
-      "description":
-        "Es un App aplicación para cinefilos, donde se puede ver la cartelera actual y los estrenos, se puede buscar peliculas en especifico y mirar la informacion de esta, tambien se puede obtener los horarios de las proyecciones en los cines cercanos.",
-      "tecnologies": "HTML, CSS, JavaScript, REACT y BOOTSTRAP.",
-      "url": "https://la-cartelera-es.netlify.app",
-      "github": "https://github.com/Luis1Gonzalez/laCarteleraEs.git",
       "picture": laCartelera,
     },
 
     {
-      "id": 5,
+      "id": 7,
       "title": "Barra Libre 45",
-      "description":
-        "Barra Libre 45 es una App donde a través del consumo del API de .thecocktaildb se muestra fichas de cocteles junto a sus ingredientes y forma de preparación, en la app se puede pasar a través de las páginas para encontrar algún coctel que se desee o se puede buscar por su nombre.",
-      "tecnologies": "HTML, CSS, JavaScript, REACT, BOOTSTRAP y SASS.",
-      "url": "https://barra-libre-45.netlify.app/",
-      "github": "https://github.com/Luis1Gonzalez/barra-libre-45",
       "picture": barralibre45,
     },
 
     {
-      "id": 6,
+      "id": 8,
       "title": "ZuccherosCakes",
-      "description":
-        "La página desarrollada para la empresa de repostería Zucheros Cake ubicada en Santiago de Chile, es de tipo informativa, muestra lo mejor de su trabajo y sus productos más recientes, al igual que ofrece la información de contacto.",
-      "tecnologies": "HTML, CSS y BOOTSTRAP",
-      "url": "https://zuccheroscakes.netlify.app/index.html",
-      "github": "https://github.com/Luis1Gonzalez/zuccheroscakes",
       "picture": zuccherosCakes,
     }
   ];
